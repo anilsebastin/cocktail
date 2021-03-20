9a70408e56c90753b0278a772469d481f9823dee
Anil Sebastin <anilsebastin@gmail.com>
2021-03-20 13:23:35 +0530

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet condersor timdfge adrfdsd gsgs vd dfs dggdbdb fbfbdg dfgdfgfdf fbbfdbd fbhkh hjk  asw qdsdvds svdv sdvv vsgvdg xfvfb gb xfbxb bgdge fbfxb sgbrebb xfbbrgdrg xfbbbd fbgnnfgn onfono iuohguhoi johohoijl ojojo ojojojo 
+      </p>
+    </section>
+  )
+}
+
+export default About
